ac7e4f92a42671d3c3cd708b40be6dcdd5dd479f
csarraud <clementsarraud@gmail.com>
2020-11-28 19:39:42 +0100

    add cooldown between each power usage

--- src/main/java/fr/sonkuun/shinobiweapon/listener/ShinobiWeaponPowerListener.java
+++ src/main/java/fr/sonkuun/shinobiweapon/listener/ShinobiWeaponPowerListener.java
@@ -7,11 +7,28 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ShinobiWeaponPowerListener {
 	
+	public static long POWER_COOLDOWN_DURATION_IN_TICKS = 20;
+	public static long LAST_POWER_USE_IN_TICKS = 0;
+	
 	@SubscribeEvent
 	public void playerTick(PlayerTickEvent event) {
-		if(KeyRegister.FIRST_ITEM_POWER.isPressed() && event.player.getHeldItemMainhand().getItem() instanceof IPoweredItem) {
+		LAST_POWER_USE_IN_TICKS += 1;
+		
+		if(KeyRegister.FIRST_ITEM_POWER.isKeyDown() && isCooldownValid()
+				&& event.player.getHeldItemMainhand().getItem() instanceof IPoweredItem) {
 			IPoweredItem poweredItem = (IPoweredItem) event.player.getHeldItemMainhand().getItem();
-			poweredItem.useFirstPower(event.player);
+			if(poweredItem.useFirstPower(event.player))
+				LAST_POWER_USE_IN_TICKS = 0;
 		}
+		else if(KeyRegister.SECOND_ITEM_POWER.isKeyDown() && isCooldownValid()
+				&& event.player.getHeldItemMainhand().getItem() instanceof IPoweredItem) {
+			IPoweredItem poweredItem = (IPoweredItem) event.player.getHeldItemMainhand().getItem();
+			if(poweredItem.useSecondPower(event.player))
+				LAST_POWER_USE_IN_TICKS = 0;
+		}
+	}
+	
+	public boolean isCooldownValid() {
+		return LAST_POWER_USE_IN_TICKS >= POWER_COOLDOWN_DURATION_IN_TICKS;
 	}
 }
