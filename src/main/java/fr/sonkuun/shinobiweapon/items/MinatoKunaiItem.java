0d287f71a220cfb2b1d18bcd618df2ca54e4c5e3
csarraud <clementsarraud@gmail.com>
2020-11-28 19:19:14 +0100

    change return value when using item power + refill item to player inventory after teleport

--- src/main/java/fr/sonkuun/shinobiweapon/items/MinatoKunaiItem.java
+++ src/main/java/fr/sonkuun/shinobiweapon/items/MinatoKunaiItem.java
@@ -1,18 +1,24 @@
 package fr.sonkuun.shinobiweapon.items;
 
+import java.util.UUID;
+
 import fr.sonkuun.shinobiweapon.capability.CapabilityShinobiWeapon;
 import fr.sonkuun.shinobiweapon.capability.ShinobiWeaponData;
 import fr.sonkuun.shinobiweapon.entity.kunai.AbstractKunaiEntity;
 import fr.sonkuun.shinobiweapon.entity.kunai.MinatoKunaiEntity;
 import fr.sonkuun.shinobiweapon.register.ItemRegister;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.Vector3d;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 
 public class MinatoKunaiItem extends AbstractKunaiItem {
-	
+
 	public MinatoKunaiItem() {
 		super();
 	}
@@ -29,26 +35,51 @@ public class MinatoKunaiItem extends AbstractKunaiItem {
 	}
 
 	@Override
-	public void useFirstPower(PlayerEntity player) {
+	public boolean useFirstPower(PlayerEntity player) {
 		/*
 		 * Shinobi Weapon capability is required to using item power
 		 */
 		ShinobiWeaponData shinobiWeaponData = player.getCapability(CapabilityShinobiWeapon.CAPABILITY_SHINOBI_WEAPON).orElse(null);
-		
+
 		if(shinobiWeaponData == null) {
-			return;
+			return false;
+		}
+
+		if(shinobiWeaponData.playerIsLookingToMinatoKunai() && player.world instanceof ServerWorld) {
+			ServerWorld serverWorld = (ServerWorld) player.world;
+			Entity entity = serverWorld.getEntityByUuid(shinobiWeaponData.getMinatoKunaiUUID());
+			
+			if(entity == null)
+				return false;
+			
+			Vec3d kunaiPos = entity.getPositionVec();
+
+			/*
+			 * Refill item in player inventory if not full
+			 */
+			for(ItemStack stack : player.inventory.mainInventory) {
+				if(stack.getItem().equals(this.getItem()) && stack.getCount() < stack.getMaxStackSize()) {
+					stack.setCount(stack.getCount() + 1);
+					entity.remove();
+					break;
+				}
+			}
+
+			if(!entity.removed && player.inventory.getFirstEmptyStack() != -1) {
+				player.inventory.add(player.inventory.getFirstEmptyStack(), new ItemStack(this.getItem()));
+				entity.remove();
+			}
+			
+			player.setPositionAndUpdate(kunaiPos.x, kunaiPos.y, kunaiPos.z);
+			return true;
 		}
 		
-		if(shinobiWeaponData.playerIsLookingToMinatoKunai()) {
-			Vec3d kunaiPos = shinobiWeaponData.getMinatoKunaiPosition();
-			player.setPositionAndUpdate(kunaiPos.x, kunaiPos.y, kunaiPos.z);
-		}
+		return false;
 	}
 
 	@Override
-	public void useSecondPower(PlayerEntity player) {
-		// TODO Auto-generated method stub
-		
+	public boolean useSecondPower(PlayerEntity player) {
+		return false;
 	}
 
 }
