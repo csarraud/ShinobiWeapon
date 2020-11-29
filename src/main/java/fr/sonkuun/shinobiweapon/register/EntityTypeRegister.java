5910e79229af34b3a9d2f5361e70dbe071651855
csarraud <clementsarraud@gmail.com>
2020-11-29 18:10:23 +0100

    add shuriken entity type and register

--- src/main/java/fr/sonkuun/shinobiweapon/register/EntityTypeRegister.java
+++ src/main/java/fr/sonkuun/shinobiweapon/register/EntityTypeRegister.java
@@ -6,6 +6,7 @@ import java.util.List;
 import fr.sonkuun.shinobiweapon.ShinobiWeapon;
 import fr.sonkuun.shinobiweapon.entity.kunai.KunaiEntity;
 import fr.sonkuun.shinobiweapon.entity.kunai.MinatoKunaiEntity;
+import fr.sonkuun.shinobiweapon.entity.shuriken.ShurikenEntity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraft.util.ResourceLocation;
@@ -31,13 +32,22 @@ public class EntityTypeRegister {
 			.setTrackingRange(20)
 			.setCustomClientFactory((spawnEntity, world) -> new MinatoKunaiEntity(world))
 			.build(new ResourceLocation(ShinobiWeapon.MODID, "minato_kunai").toString());
+	
+	public static final EntityType<ShurikenEntity> SHURIKEN = EntityType.Builder.<ShurikenEntity>create(ShurikenEntity::new, EntityClassification.MISC)
+			.size(0.4f, 0.4f)
+			.setShouldReceiveVelocityUpdates(true)
+			.setTrackingRange(20)
+			.setCustomClientFactory((spawnEntity, world) -> new ShurikenEntity(world))
+			.build(new ResourceLocation(ShinobiWeapon.MODID, "shuriken").toString());
 
 	static {
 		KUNAI.setRegistryName(ShinobiWeapon.MODID, "kunai_entity");
 		MINATO_KUNAI.setRegistryName(ShinobiWeapon.MODID, "minato_kunai_entity");
+		SHURIKEN.setRegistryName(ShinobiWeapon.MODID, "shuriken_entity");
 
 		ENTITY_TYPES.add(KUNAI);
 		ENTITY_TYPES.add(MINATO_KUNAI);
+		ENTITY_TYPES.add(SHURIKEN);
 	}
 
 	public static void init() {
