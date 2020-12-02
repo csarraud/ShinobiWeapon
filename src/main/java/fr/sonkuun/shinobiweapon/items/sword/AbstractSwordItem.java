451bb884b98ce996fff2ec93905fbd3980c8cd48
csarraud <clementsarraud@gmail.com>
2020-12-02 16:38:44 +0100

    override IPoweredItem method

--- src/main/java/fr/sonkuun/shinobiweapon/items/sword/AbstractSwordItem.java
+++ src/main/java/fr/sonkuun/shinobiweapon/items/sword/AbstractSwordItem.java
@@ -4,11 +4,30 @@ import fr.sonkuun.shinobiweapon.items.IPoweredItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemTier;
 import net.minecraft.item.SwordItem;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
 
 public abstract class AbstractSwordItem extends SwordItem implements IPoweredItem {
 
 	public AbstractSwordItem(int maxDamage, float attackDamage) {
 		super(ItemTier.DIAMOND, maxDamage, attackDamage, new Properties().group(ItemGroup.COMBAT).maxStackSize(1));
+	}
+
+	@Override
+	public void playerDamagedLivingEntity(LivingDamageEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void livingEntityDamagedPlayer(LivingDamageEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void environmentDamagedPlayer(LivingDamageEvent event) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
