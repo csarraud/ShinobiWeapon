c6d5c567735e7fc4a83a0a74acde959824f61ae8
csarraud <clementsarraud@gmail.com>
2020-12-09 18:21:56 +0100

    add blank scroll item and recipe

---
+++ src/main/java/fr/sonkuun/shinobiweapon/items/scroll/BlankScrollItem.java
@@ -0,0 +1,12 @@
+package fr.sonkuun.shinobiweapon.items.scroll;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class BlankScrollItem extends Item {
+
+	public BlankScrollItem() {
+		super(new Properties().group(ItemGroup.MISC).maxStackSize(64));
+	}
+
+}
--- src/main/java/fr/sonkuun/shinobiweapon/register/ItemRegister.java
+++ src/main/java/fr/sonkuun/shinobiweapon/register/ItemRegister.java
@@ -6,6 +6,7 @@ import java.util.List;
 import fr.sonkuun.shinobiweapon.ShinobiWeapon;
 import fr.sonkuun.shinobiweapon.items.kunai.KunaiItem;
 import fr.sonkuun.shinobiweapon.items.kunai.MinatoKunaiItem;
+import fr.sonkuun.shinobiweapon.items.scroll.BlankScrollItem;
 import fr.sonkuun.shinobiweapon.items.shuriken.ShurikenItem;
 import fr.sonkuun.shinobiweapon.items.sword.JashinScytheItem;
 import net.minecraft.item.Item;
@@ -22,17 +23,20 @@ public class ItemRegister {
 	public static final Item MINATO_KUNAI = new MinatoKunaiItem();
 	public static final Item SHURIKEN = new ShurikenItem();
 	public static final Item JASHIN_SCYTHE = new JashinScytheItem();
+	public static final Item BLANK_SCROLL = new BlankScrollItem();
 
 	static {
 		KUNAI.setRegistryName(ShinobiWeapon.MODID, "kunai");
 		MINATO_KUNAI.setRegistryName(ShinobiWeapon.MODID, "minato_kunai");
 		SHURIKEN.setRegistryName(ShinobiWeapon.MODID, "shuriken");
 		JASHIN_SCYTHE.setRegistryName(ShinobiWeapon.MODID, "jashin_scythe");
+		BLANK_SCROLL.setRegistryName(ShinobiWeapon.MODID, "blank_scroll");
 
 		ITEMS.add(KUNAI);
 		ITEMS.add(MINATO_KUNAI);
 		ITEMS.add(SHURIKEN);
 		ITEMS.add(JASHIN_SCYTHE);
+		ITEMS.add(BLANK_SCROLL);
 	}
 
 	public static void init() {
